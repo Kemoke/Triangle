b289d5166a2d3985e9fec3dcecd116a8ac451a13
Kemoke <mail@kemoke.net>
2017-04-15 19:40:19 +0200

    Initial commit. Add test scaffold.

---
+++ TriangleTest/TriangleTest.cs
@@ -0,0 +1,100 @@
+using System;
+using Xunit;
+using Triangle;
+
+namespace TriangleTest
+{
+    public class TriangleTest
+    {
+        [Fact]
+        public void TestIsTriangleTrue()
+        {
+            Assert.True(false);
+        }
+
+        [Fact]
+        public void TestIsTriangleFalse()
+        {
+            Assert.True(false);
+        }
+
+        [Fact]
+        public void TestIsTriangleBadInput()
+        {
+            Assert.True(false);
+        }
+
+        [Fact]
+        public void TestIsEquilateralTrue()
+        {
+            Assert.True(false);
+        }
+
+        [Fact]
+        public void TestIsEquilateralFalse()
+        {
+            Assert.True(false);
+        }
+
+        [Fact]
+        public void TestIsEquilateralBadInput()
+        {
+            Assert.True(false);
+        }
+
+        [Fact]
+        public void TestIsRightAngledTrue()
+        {
+            Assert.True(false);
+        }
+
+        [Fact]
+        public void TestIsRightAngledFalse()
+        {
+            Assert.True(false);
+        }
+
+        [Fact]
+        public void TestIsRightAngledBadInput()
+        {
+            Assert.True(false);
+        }
+
+        [Fact]
+        public void TestIsBluntAngledTrue()
+        {
+            Assert.True(false);
+        }
+
+        [Fact]
+        public void TestIsBluntAngledFalse()
+        {
+            Assert.True(false);
+        }
+
+        [Fact]
+        public void TestIsBluntAngledBadInput()
+        {
+            Assert.True(false);
+        }
+
+        [Fact]
+        public void TestIsSharpAngledTrue()
+        {
+            Assert.True(false);
+        }
+
+        [Fact]
+        public void TestIsSharpAngledFalse()
+        {
+            Assert.True(false);
+        }
+
+        [Fact]
+        public void TestIsSharpAngledBadInput()
+        {
+            Assert.True(false);
+        }
+
+    }
+}
